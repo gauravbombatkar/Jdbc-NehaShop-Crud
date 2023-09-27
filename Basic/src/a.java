06959388f07d9e27b30c0dfea0ba3ede7273cb44
bprerna <bodadeprerna@gmail.com>
2023-09-27 10:49:04 +0530

    first commit

---
+++ Basic/src/a.java
@@ -0,0 +1,20 @@
+
+public class a {
+	public static void main(String[] args) {
+		
+		// two types of method length,length()
+		int [] a= new int[10];
+		System.out.println(a.length);
+		
+		String s = "chor";
+		System.out.println(s.length());
+		
+		a[0] = 'a';
+	
+		
+		
+				
+				   
+	}
+
+}
